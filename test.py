703851044a8ddcc1dfd35762fab1df71e3961372
VISWA68 <you@example.com>
2025-05-11 19:47:24 +0530

    Made changes

--- test.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoTokenizer, T5ForConditionalGeneration
-
-tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-base")
-model = T5ForConditionalGeneration.from_pretrained("Salesforce/codet5-base")
-
-print(" Model and tokenizer loaded successfully")
